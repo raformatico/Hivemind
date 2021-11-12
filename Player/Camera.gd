d7d8a2122d68b70d5c76567d876e95057eb855f4
raformatico <raformatico@raformatico.com>
2021-11-12 21:51:43 +0100

    3d movement with camera

--- Player/Camera.gd
+++ Player/Camera.gd
@@ -20,23 +20,31 @@ export (float, 0.001, 0.1) var mouse_sensitivity = 0.003
 export (bool) var invert_y = false
 export (bool) var invert_x = false
 
+export var joystick_sensitivity = 1
+
+var direction
+
+func get_global_transformation() -> Vector3:
+	return camera.global_transform
+
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event):
-	if Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
-		return
+	direction = 1 if invert_y else -1
 	if event is InputEventMouseMotion:
 		if event.relative.x != 0:
-			var dir = 1 if invert_x else -1
-			rotate_object_local(Vector3.UP, dir * event.relative.x * mouse_sensitivity)
+			rotate_object_local(Vector3.UP, direction * event.relative.x * mouse_sensitivity)
 		if event.relative.y != 0:
-			var dir = 1 if invert_y else -1
 			#var y_rotation = clamp(event.relative.y, -30, 30)
 			#camera_in.rotate_object_local(Vector3.RIGHT, dir * y_rotation * mouse_sensitivity)
-			camera_in.rotate_object_local(Vector3.RIGHT, dir * event.relative.y * mouse_sensitivity)
+			camera_in.rotate_object_local(Vector3.RIGHT, direction * event.relative.y * mouse_sensitivity)
 
 func _process(delta):
 	camera_in.rotation.x = clamp(camera_in.rotation.x, deg2rad(up_limit), deg2rad(low_limit))
-	if target:
-		global_transform.origin = get_node(target).global_transform.origin
+	#get joystick
+	rotate_y(deg2rad((Input.get_action_strength("rotate_left") - Input.get_action_strength("rotate_right")) * joystick_sensitivity))
+	camera_in.rotate_x(direction * deg2rad((Input.get_action_strength("rotate_up") - Input.get_action_strength("rotate_down")) * joystick_sensitivity))
+	camera_in.rotation.x = clamp(camera_in.rotation.x, deg2rad(up_limit), deg2rad(low_limit))
+	#if target:
+		#global_transform.origin = get_node(target).global_transform.origin
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,53 +1,69 @@
 extends KinematicBody
 class_name Player
 
-var speed = 7
-const ACCEL_DEFAULT = 10
-const ACCEL_AIR = 1
-onready var accel = ACCEL_DEFAULT
-var gravity = 9.8
-var jump = 3
+export var acceleration_floor = 10
+export var acceleration_air = 1
+export var angular_velocity = 30
+export var gravity = 9.8
+export var jump = 3
+export var camera_acceleration = 40
+export var mouse_sense = 0.1
+export var joystick_sense = 1
+export var up_limit := -55
+export var low_limit := 14
+export var speed = 7
 
 var snap
-
 var direction = Vector3()
 var velocity = Vector3()
-var gravity_vec = Vector3()
+var gravity_vector = Vector3()
 var movement = Vector3()
 
-# Called when the node enters the scene tree for the first time.
+var acceleration = acceleration_floor
+onready var body := $Body
+onready var camera := $Camera
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	body.set_as_toplevel(true)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _process(delta: float) -> void:
+	# Translate body to follow KinematicBody
+	body.global_transform.origin = global_transform.origin
+	# Rotate body to point to the movement
+	if direction != Vector3.ZERO:
+		body.rotation.y = lerp_angle(body.rotation.y, atan2(-direction.x, -direction.z), angular_velocity * delta)
 
 func _physics_process(delta: float) -> void:
+	
 	#get keyboard input
 	direction = Vector3.ZERO
-	var h_rot = global_transform.basis.get_euler().y
+	#TODO review move_right and left
+	var h_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	var f_input = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
-	var h_input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	direction = Vector3(h_input, 0, f_input).rotated(Vector3.UP, h_rot).normalized()
+	var direction_keys = Vector3(h_input, 0, f_input)
+	# Get radians of camera rotation around Y axis
+	var camera_rotation = camera.get_global_transformation().basis.get_euler().y
+	# Rotate keys around Y by camera rotation
+	direction = direction_keys.rotated(Vector3.UP, camera_rotation).normalized()
 	
 	#jumping and gravity
 	if is_on_floor():
 		snap = -get_floor_normal()
-		accel = ACCEL_DEFAULT
-		gravity_vec = Vector3.ZERO
+		acceleration = acceleration_floor
+		gravity = Vector3.ZERO
 	else:
 		snap = Vector3.DOWN
-		accel = ACCEL_AIR
-		gravity_vec += Vector3.DOWN * gravity * delta
+		acceleration = acceleration_air
+		gravity_vector += Vector3.DOWN * gravity * delta
 		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		snap = Vector3.ZERO
-		gravity_vec = Vector3.UP * jump
+		gravity_vector = Vector3.UP * jump
 	
 	#make it move
-	velocity = velocity.linear_interpolate(direction * speed, accel * delta)
-	movement = velocity + gravity_vec
+	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+	movement = velocity + gravity_vector
 	
 	move_and_slide_with_snap(movement, snap, Vector3.UP)
