538a40182fdc8ec93b4488da8fa83ff24199578a
matildagame <55888362+matildagame@users.noreply.github.com>
2021-11-30 00:29:16 +0100

    Sound Area, Lynph animation, Buggati animations improved.

---
+++ World/zones/LarvaeZone.gd
@@ -0,0 +1,25 @@
+extends Area
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export var in_song="larvas"
+export var out_song="paisaje"
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_LarvaeZone_body_entered(body):
+	AudioEngine.crossfade_to_song(in_song, 2)
+
+
+func _on_LarvaeZone_body_exited(body):
+	AudioEngine.crossfade_to_song(out_song, 2)
